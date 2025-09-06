e7f58f078153081adcad3f7e0b7adae1622f4747
TiborBalintVida <vida.tibor.balint@gmail.com>
2025-09-06 23:04:45 +0200

    RealTimeChart set to MeasuredTrailingPeriods

--- src/components/RealTimeChart.jsx
+++ src/components/RealTimeChart.jsx
@@ -31,59 +31,227 @@ const RealTimeChart = ({
 
     // Fetch chart data
     const fetchChartData = async () => {
-        if (!device || !hmacKey || !startDate || !endDate) return;
+        if (!device || !hmacKey || !startDate || !endDate || device.application !== 'Continuous') return;
         try {
             const isBidirectional = device.direction === 'BiDirectional';
+            const isLowerIsBetter = device.direction === 'LowerIsBetter';
 
-            const start = encodeURIComponent(toLocalTime(startDate).toISOString().substring(0, 19));
             const end = encodeURIComponent(toLocalTime(endDate).toISOString().substring(0, 19));
-            const url = `https://precog.vidasoftapi.com/api/Continuous/MeasuredDateRange?DeviceId=${device.deviceId}&StartDate=${start}&EndDate=${end}`;
+            let url = '';
+            url = isBidirectional
+                ? url = `https://precog.vidasoftapi.com/api/BiDirectionalContinuous/MeasuredTrailingPeriods?DeviceId=${device.deviceId}&EndDate=${end}&Periods=40`
+                : url = `https://precog.vidasoftapi.com/api/Continuous/MeasuredTrailingPeriods?DeviceId=${device.deviceId}&EndDate=${end}&Periods=40`;
             const response = await fetch(url, {
                 headers: {
                     accept: '*/*',
                     HMAC_Key: hmacKey,
                 },
             });
+
             if (!response.ok) {
                 setError('Failed to load chart data');
                 setChartData(null);
                 return;
             }
             const data = await response.json();
-            setChartData({
-                labels: data.map((item) => item.measuredAt),
-                datasets: [
+
+            const output =
+                device.application === 'Continuous'
+                    ? data
+                    : data[0]?.outputData || [];
+
+            const datasets = [];
+
+            if (isBidirectional) {
+                datasets.push(
                     {
-                        label: 'Actual',
-                        data: data.map((item) => item.actual),
-                        borderColor: '#014F91',
-                        backgroundColor: '#014F91',
-                        tension: 0.2,
-                        pointRadius: small ? 0 : 2,
-                        borderWidth: 2,
+                        label: 'CriticalAbove',
+                        data: output.map((item) => item.anomalyAbove === 2 ? item.actual : null),
+                        borderWidth: 0,
+                        backgroundColor: 'red',
+                        tension: 0,
+                        pointRadius: 10,
+                        pointHoverRadius: 40,
+                        tension: 0,
+                    },
+                    {
+                        label: 'WarningAbove',
+                        data: output.map((item) => item.anomalyAbove === 1 ? item.actual : null),
+                        borderWidth: 0,
+                        backgroundColor: 'lightBlue',
+                        tension: 0,
+                        pointRadius: 8,
+                        pointHoverRadius: 40,
+                        tension: 0,
+                    },
+                    {
+                        label: 'CriticalBelow',
+                        data: output.map((item) => item.anomalyBelow === 2 ? item.actual : null),
+                        borderWidth: 0,
+                        backgroundColor: 'blue',
+                        tension: 0,
+                        pointRadius: 10,
+                        pointHoverRadius: 40,
+                        tension: 0,
+                    },
+                    {
+                        label: 'WarningBelow',
+                        data: output.map((item) => item.anomalyBelow === 1 ? item.actual : null),
+                        borderWidth: 0,
+                        backgroundColor: 'gold',
+                        tension: 0,
+                        pointRadius: 8,
+                        pointHoverRadius: 40,
+                        tension: 0,
+                    },
+                    {
+                        label: 'Tolerance Above',
+                        data: output.map((item) => item.targetAbove + item.toleranceAbove),
+                        backgroundColor: 'white',
+                        borderColor: 'darkBlue',
+                        borderDash: [5, 5],
+                        pointRadius: 0,
+                        borderWidth: 1.5,
+                        tension: 0,
+                    },
+                    {
+                        label: 'Target Above',
+                        data: output.map((item) => item.targetAbove),
+                        backgroundColor: 'white',
+                        borderColor: 'lightBlue',
+                        borderDash: [5, 5],
+                        pointRadius: 0,
+                        borderWidth: 1.5,
+                        tension: 0,
+                    },
+                    {
+                        label: 'Target Below',
+                        data: output.map((item) => item.targetBelow),
+                        backgroundColor: 'white',
+                        borderColor: 'Gold',
+                        borderDash: [5, 5],
+                        pointRadius: 0,
+                        borderWidth: 1.5,
+                        tension: 0,
+                    },
+                    {
+                        label: 'Tolerance Below',
+                        data: output.map((item) => item.targetBelow - item.toleranceBelow),
+                        backgroundColor: 'white',
+                        borderColor: 'red',
+                        borderDash: [5, 5],
+                        pointRadius: 0,
+                        borderWidth: 1.5,
+                        tension: 0,
+                    }
+                );
+            } else {
+                datasets.push(
+                    {
+                        label: 'Critical',
+                        data: output.map((item) => item.anomaly === 2 ? item.actual : null),
+                        borderWidth: 0,
+                        backgroundColor: 'red',
+                        tension: 0,
+                        pointRadius: 10,
+                        pointHoverRadius: 40,
+                        tension: 0,
+                    },
+                    {
+                        label: 'Warning',
+                        data: output.map((item) => item.anomaly === 1 ? item.actual : null),
+                        borderWidth: 0,
+                        backgroundColor: 'gold',
+                        tension: 0,
+                        pointRadius: 8,
+                        pointHoverRadius: 40,
+                        tension: 0,
                     },
                     {
                         label: 'Target',
-                        data: data.map((item) => item.target),
-                        borderColor: 'gold',
+                        data: output.map((item) => item.target),
+                        backgroundColor: 'white',
+                        borderColor: 'Gold',
                         borderDash: [5, 5],
                         pointRadius: 0,
-                        borderWidth: 1,
+                        borderWidth: 4,
                         tension: 0,
                     },
                     {
                         label: 'Tolerance',
-                        data: data.map((item) => item.target + item.tolerance),
+                        data: output.map((item) => isLowerIsBetter ? item.target + item.tolerance : item.target - item.tolerance),
                         borderColor: 'red',
                         borderDash: [5, 5],
                         pointRadius: 0,
-                        borderWidth: 1,
+                        borderWidth: 4,
                         tension: 0,
                     },
-                ],
+                );
+            }
+
+
+            datasets.push(
+                {
+                    label: 'Actual',
+                    data: output.map((item) => item.actual),
+                    borderColor: '#014F91',
+                    backgroundColor: output.map((item) => (item.anomaly === 0 || item.anomalyAbove === 0 || item.anomalyBelow === 0) ? '#014F91' : 'rgba(1, 79, 145, 0.5)'),
+                    tension: 0.2,
+                    pointRadius: output.map((item) => item.anomaly === 0 ? 8 : 6),
+                    pointHoverRadius: output.map((item) => item.anomaly === 0 ? 40 : 30),
+                },
+                {
+                    label: 'UCL3',
+                    data: output.map((item) => item.upperControlLimit3S),
+                    borderColor: 'grey',
+                    borderDash: [30, 15],
+                    pointRadius: 0,
+                    borderWidth: 1.5,
+                    tension: 0.1,
+                    hidden: isModalOpen ? false : true,
+                },
+                {
+                    label: 'UCL2',
+                    data: output.map((item) => item.upperControlLimit2S),
+                    borderColor: 'lightGrey',
+                    backgroundColor: '#f5f5f5',
+                    borderDash: [30, 15],
+                    pointRadius: 0,
+                    borderWidth: 1.5,
+                    tension: 0.1,
+                    hidden: isModalOpen ? false : true,
+                    fill: '-1',
+                },
+                {
+                    label: 'LCL2',
+                    data: output.map((item) => item.lowerControlLimit2S),
+                    borderColor: 'lightGrey',
+                    borderDash: [30, 15],
+                    pointRadius: 0,
+                    borderWidth: 1.5,
+                    tension: 0.1,
+                    hidden: isModalOpen ? false : true,
+                },
+                {
+                    label: 'LCL3',
+                    data: output.map((item) => item.lowerControlLimit3S),
+                    borderColor: 'grey',
+                    backgroundColor: '#f5f5f5',
+                    borderDash: [30, 15],
+                    pointRadius: 0,
+                    borderWidth: 1.5,
+                    tension: 0.1,
+                    hidden: isModalOpen ? false : true,
+                    fill: '-1',
+                },
+            );
+
+            setChartData({
+                labels: output.map((item) => item.measuredAt),
+                datasets,
             });
             setError('');
-        } catch {
+        } catch (err) {
             setError('Failed to load chart data');
             setChartData(null);
         }
